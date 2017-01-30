5fd5dbdf80e25be6fb97adbdb457abc85cecd2e5
zlatinov <zlatinov.z@gmail.com>
2017-01-30 19:22:35 +0200

    init

---
+++ CurrencyToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.8;
+
+import "./StandardToken.sol";
+
+contract CurrencyToken is StandardToken {
+
+    string public name;
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+
+    function CurrencyToken(
+        uint256 _initialAmount,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol
+        ) {
+        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
+        totalSupply = _initialAmount;                        // Update total supply
+        name = _tokenName;                                   // Set the name for display purposes
+        decimals = _decimalUnits;                            // Amount of decimals for display purposes
+        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+    }
+}
