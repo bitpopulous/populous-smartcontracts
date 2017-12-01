93bbf2527c23e39d962ef3ff7833cff9f246a76e
najienka <nemitari.ajienka@brunel.ac.uk>
2017-12-01 09:14:13 +0000

    migration fix

--- migrations/6_DepositContractsManager.js
+++ migrations/6_DepositContractsManager.js
@@ -1,24 +1,23 @@
 var
-    AccessManager = artifacts.require("AccessManager"),
-    DepositContractsManager = artifacts.require("DepositContractsManager"),
-    Populous = artifacts.require("Populous");
+AccessManager = artifacts.require("AccessManager"),
+DepositContractsManager = artifacts.require("DepositContractsManager"),
+Populous = artifacts.require("Populous");
 
 module.exports = function(deployer) {
-    var P;
+var P;
 
-    deployer.then(function() {
-        return AccessManager.deployed().then(function(AM) {
-            return deployer.deploy(DepositContractsManager, AM.address);
-        }).then(function() {
-            return Populous.deployed();
-        }).then(function(instance) {
-            P = instance;
-
-            return DepositContractsManager.deployed();
-        }).then(function(DCM) {
-            return P.setDCM(DCM.address);
-        }).then(function() {
-            console.log('Finished deploying DCM');
-        });
+deployer.then(function() {
+    return AccessManager.deployed().then(function(AM) {
+        return deployer.deploy(DepositContractsManager, AM.address);
+    }).then(function() {
+        return Populous.deployed();
+    }).then(function(instance) {
+        P = instance;
+        return DepositContractsManager.deployed();
+    }).then(function(DCM) {
+        return P.setDCM(DCM.address);
+    }).then(function() {
+        console.log('Finished deploying DCM');
     });
+});
 };
