523431870a477c54eb36f511bc6199d03a0f92d9
Nemitari Ajienka <najienka@nemitaris-mbp.home>
2017-12-27 12:35:14 +0000

    testing getPaidAmount() and getWinnerGroupIndex() in crowdsale.sol

--- truffle.js
+++ truffle.js
@@ -11,6 +11,8 @@ module.exports = {
             gas: 3500000
         },
         /* "ropsten": {
+            // geth (go-ethereum client) instance on local PC 
+            // and open RPC Port synced with ropsten blockchain network
             // geth --testnet --rpc --rpccorsdomain * --bootnodes "enode://20c9ad97c081d63397d7b685a412227a40e23c8bdc6688c6f37e97cfbc22d2b4d1db1510d8f61e6a8866ad7f0e17c02b14182d37ea7c3c8b9c2683aeb6b733a1@52.169.14.227:30303,enode://6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d@13.84.180.240:30303" console
             network_id: 3,
             host: "localhost",
@@ -21,7 +23,7 @@ module.exports = {
         }, */
         "ropsten": {
             // geth (go-ethereum client) instance on AWS with external IP 
-            // and open RPC Port
+            // and open RPC Port synced with ropsten blockchain network
             network_id: 3,
             host: "35.177.79.70",
             port: 8545,
